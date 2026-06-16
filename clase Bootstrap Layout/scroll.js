4fd0e6dd9de3ace8ec215b205e7535e515f3ebf5
JerssonRamirez <jerssonramirez38@gmail.com>
2026-06-15 23:28:38 -0400

    Update scroll.js

--- clase Bootstrap Layout/scroll.js
+++ clase Bootstrap Layout/scroll.js
@@ -1,7 +1,5 @@
 window.addEventListener('load', function() {
 
-    // --- PROTECCIÓN CONTRA CAÍDAS DE SCROLLREVEAL ---
-    // Solo ejecuta las animaciones si la librería está cargada en la página actual
     if (typeof ScrollReveal !== 'undefined') {
         
         if (document.getElementById('boletin-animado')) {
@@ -40,19 +38,17 @@ window.addEventListener('load', function() {
             observeParents: true
         });
 
-        // Slider de miniaturas pequeñas
         var thumbs = new Swiper('.gallery-thumbs', {
             slidesPerView: 'auto',
             spaceBetween: 10,
             centeredSlides: true,
             loop: true,
-            loopedSlides: 6, // Crucial mantener el mismo valor para evitar bloqueos
+            loopedSlides: 6, 
             slideToClickedSlide: true,
             observer: true,
             observeParents: true
         });
 
-        // Sincronización perfecta entre ambos elementos
         slider.controller.control = thumbs;
         thumbs.controller.control = slider;
     }
